88b8c95105f1f4c135f5af3e044bf83a821d2188
SpoorthyPawar <spurthy02199@gmail.com>
2022-11-26 11:55:40 +0100

    Added tests for multiple inputs

--- src/taxCalculator.py
+++ src/taxCalculator.py
@@ -1,6 +1,32 @@
+class TaxData:
+    def __init__(self):
+        self.total_price = 0.0
+        self.sales_tax = 0.0
+        self.inputlist = []
+
+    def get_total_price(self):
+        return self.total_price
+
+    def set_total_price(self, total_price):
+        self.total_price = total_price
+
+    def get_sales_tax(self):
+        return self.sales_tax
+
+    def set_sales_tax(self, sales_tax):
+        self.sales_tax = sales_tax
+
+    def get_inputlist(self):
+        return self.inputlist
+
+    def set_inputlist(self, inputlist):
+        self.inputlist = inputlist
 
 def importTaxCalculator(item_price):
     return 0.0
 
 def basicTaxCalculator(item_price):
     return 0.0
+
+def tax_calculator(input_item_list):
+    return None
